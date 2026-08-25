a5e2f704e45f82304144ba36eaf46d1f4ea61c3d
Adams <owoadeboluwatife3@gmail.com>
2026-08-25 13:09:17 +0100

    i have reached if statement

---
+++ index.js
@@ -0,0 +1,6 @@
+console.log('Hello gangs')
+console.log('I am Adams')
+console.log('I wanna be stinking rich')
+//variables
+let Name = "Adams"
+console.log(Name)
---
+++ java.js
@@ -0,0 +1,73 @@
+let name = "Adams";
+console.log(name)
+let school = "Uniosun"
+let age = 17
+console.log(school)
+console.log(age)
+console.log("my name is " + name + " I am " + age + " years old")
+console.log(`my name is ${name} and i am ${age} years old`)
+let player = "mbappe"
+let goals = 2
+console.log(player)
+console.log(goals)
+goals = 3
+console.log(goals)
+//little work 
+name = "bolu"
+age = 20
+school = "osun"
+let course = "software enginering"
+
+console.log(`my name is ${name} i am ${age} years old, schooling at ${school} studying $S{course}`)
+
+//calculation
+goals = 4
+let assists = 2
+let total = goals + assists
+console.log(total)
+
+let a = 10 
+let b = 2 
+console.log(a + b)
+console.log(a - b)
+console.log(a / b)
+console.log(a * b)
+console.log(10 % 3)
+
+//mini project
+player = "adams"
+goals = 3
+assists = 2
+
+let goalpoints = goals * 3
+let assistpoints = assists
+let totalpoints = goals + assists
+
+console.log(`player : ${player}`)
+console.log(`assists : ${assistpoints}`)
+console.log(`goals : ${goalpoints}`)
+console.log(`total points  : ${totalpoints}`)
+
+//practice 
+name = "Lildamz"
+age = 1010
+let department = "software engineering"
+let favclub = "manchester city"
+let favplayer = "neymar"
+goals = 5
+assists = 4
+// let contribution = goals + assists
+console.log(`name : ${name}`)
+console.log(`age : ${age}`)
+console.log(`department : ${department}`)
+console.log(`fav club : ${favclub}`)
+console.log(`fav player : ${favplayer}`)
+console.log(`g/a : ${goals + assists}`)
+
+//if 
+
+age = 19
+if (age >= 18){
+    console.log("you can vote ");
+    
+}
